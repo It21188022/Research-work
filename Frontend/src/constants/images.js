79b3abed5ded1f1479205bd2f667e5fbc6023854
IT21346286 <it21346286@my.sliit.lk>
2024-10-23 07:57:42 +0530

    Add images constant

---
+++ Frontend/src/constants/images.js
@@ -0,0 +1,12 @@
+import Logo1 from "../assets/Logo1.png";
+import Poster from "../assets/Poster.png";
+import tgreen from "../assets/tgreen.png";
+
+
+const images = {
+  Logo1,
+  tgreen,
+  Poster,
+};
+
+export default images;
